b0be2d59aafe9dcbd7041199d6fd152b2d4a3a46
Booster39 <abdiame10@yahoo.fr>
2024-06-12 17:14:52 +0200

    feat: Participate and noLongerParticipate tests

--- back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -1,10 +1,13 @@
 package com.openclassrooms.starterjwt.services;
 
+import com.openclassrooms.starterjwt.exception.BadRequestException;
+import com.openclassrooms.starterjwt.exception.NotFoundException;
 import com.openclassrooms.starterjwt.models.Session;
 import com.openclassrooms.starterjwt.models.Teacher;
 import com.openclassrooms.starterjwt.models.User;
 import com.openclassrooms.starterjwt.repository.SessionRepository;
 import com.openclassrooms.starterjwt.repository.UserRepository;
+import com.sun.jdi.LongValue;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
@@ -12,14 +15,17 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.time.LocalDateTime;
-import java.util.Date;
-import java.util.List;
+import java.util.*;
 
+import static com.fasterxml.jackson.databind.type.LogicalType.Array;
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.verify;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class SessionServiceTest {
@@ -35,7 +41,31 @@ class SessionServiceTest {
     private Teacher teacher;
 
     @Mock
-    private List<User> userList;
+    private User user;
+
+   User user1 = User.builder()
+            .id(1L)
+            .email("user1@example.com")
+        .lastName("Smith")
+        .firstName("John")
+        .password("password1")
+        .admin(false).build();
+
+    User user2 = User.builder()
+            .id(2L)
+            .email("user2@example.com")
+            .lastName("Doe")
+            .firstName("Jane")
+            .password("password2")
+            .admin(false).build();
+
+    public List<User> userList = new ArrayList<User>();
+    public static List<User> createUsers(List<User> userList, User user1, User user2) {
+        userList.add(user1);
+        userList.add(user2);
+        return userList;
+    }
+
 
     @Mock
     private Date date;
@@ -50,10 +80,12 @@ class SessionServiceTest {
             .teacher(teacher)
             .description("description")
             .name("Lisa")
-            .users(userList)
+            .users(createUsers(userList, user1, user2))
             .date(date)
             .build();
 
+    //@Mock
+    //private SessionService s
     @Test
     void checkIfFindAll() {
         //when
@@ -120,14 +152,58 @@ class SessionServiceTest {
     }
 
     @Test
-    @Disabled
-    void checkIfParticipate() {
+    void checkIfParticipateReturnNotFoundException() {
+        //given
+        sessionRepository.save(session);
+        Long sessionId = session.getId();
+        long userId = user.getId();
 
+        assertThrows(NotFoundException.class, () -> underTest.participate(sessionId, userId));
+    }
+
+   @Test
+    void checkIfParticipateBadRequestException() {
+       Long sessionId = 1L;
+       Long userId = 2L;
+
+       User mockUser = mock(User.class);
+       when(mockUser.getId()).thenReturn(userId);
+
+       List<User> users = Arrays.asList(mockUser);
+       Session mockSession = mock(Session.class);
+       when(mockSession.getUsers()).thenReturn(users);
+       when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(mockSession));
+       when(userRepository.findById(userId)).thenReturn(Optional.of(mockUser));
+       assertThrows(BadRequestException.class, () -> underTest.participate(sessionId, userId));
+
+   }
+
+
+
+    @Test
+    void checkIfNoLongerParticipateThrowsNotFoundException() {
+        //given
+        sessionRepository.save(session);
+        Long sessionId = session.getId();
+        long userId = user.getId();
+        // when
+        //then
+        assertThrows(NotFoundException.class, ()->{underTest.noLongerParticipate(sessionId, userId);});
     }
 
     @Test
-    @Disabled
-    void checkIfNoLongerParticipate() {
+    void checkIfNoLongerParticipateBadRequestException() {
+        //given
+        Long sessionId = 1L;
+        Long userId = 2L;
 
+        User mockUser = mock(User.class);
+        when(mockUser.getId()).thenReturn(userId + 1);
+
+        List<User> users = Arrays.asList(mockUser);
+        Session mockSession = mock(Session.class);
+        when(mockSession.getUsers()).thenReturn(users);
+        when(sessionRepository.findById(sessionId)).thenReturn(Optional.of(mockSession));
+        assertThrows(BadRequestException.class, ()->{underTest.noLongerParticipate(sessionId, userId);});
     }
 }
